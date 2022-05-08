60fc2fb5de6c14bc46612ef24e351a25c67bfbec
Carolina Nonato <csnonato@gmail.com>
2022-05-08 17:42:08 -0400

    buttom component

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button-Comp",
+  props: {
+    text: String,
+    color: String,
+  },
+};
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,20 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
+    <Button text="Add Text" color="green" />
   </header>
 </template>
 
 <script>
+import Button from "./Button";
+
 export default {
   name: "Header-Comp",
   props: {
     title: String,
+  },
+  components: {
+    Button,
   },
 };
 </script>
